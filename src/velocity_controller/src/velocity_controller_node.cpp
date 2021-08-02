63e028d28efe5f1807bc4b0192d0c9d1b3566803
iago <polarbolide@gmail.com>
2021-08-02 23:48:46 +0200

    feat: Create velocity controller node

---
+++ src/velocity_controller/src/velocity_controller_node.cpp
@@ -0,0 +1,80 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "std_msgs/Int8.h"
+#include "std_msgs/Float64.h"
+
+using std_msgs::Float64;
+using std_msgs::Int8;
+using geometry_msgs::Twist;
+using ros::NodeHandle;
+using ros::Publisher;
+using ros::Subscriber;
+
+// kUp - increase linear speed
+// kDown - decrease linear speed
+// kLeft - increase angular speed
+// kRight - decrease angular speed
+// kQ - zero linear speed
+// kA - zero angular speed
+enum class Command : int8_t{kNone = -1, kUp, kDown, kLeft, kRight, kQ, kA};
+
+class VelocityController{
+private:
+    Twist twist_;
+    Twist d_twist_;
+    Publisher pub_;
+public:
+    VelocityController(float linear, float angular, float d_linear, float d_angular, Publisher pub){
+        d_twist_.linear.x = linear;
+        twist_.angular.z = angular;
+        d_twist_.linear.x = d_linear;
+        d_twist_.angular.z = d_angular;
+        pub_ = std::move(pub);
+    };
+
+    void JoystickCallback(std_msgs::Int8 msg){
+        Command cmd = static_cast<Command>(msg.data);
+        switch (cmd){
+            case Command::kUp:
+                if (twist_.linear.x <= 1 - d_twist_.linear.x)
+                    twist_.linear.x += d_twist_.linear.x;
+            break;
+            case Command::kDown:
+                if (twist_.linear.x >= -1 + d_twist_.linear.x)
+                    twist_.linear.x -= d_twist_.linear.x;
+            break;
+            case Command::kLeft:
+                if (twist_.angular.z <= 1 - d_twist_.angular.z)
+                    twist_.angular.z += d_twist_.angular.z;
+            break;
+            case Command::kRight:
+                if (twist_.angular.z >= -1 + d_twist_.angular.z)
+                    twist_.angular.z -= d_twist_.angular.z;
+            break;
+            case Command::kQ: {
+                twist_.linear.x = 0;
+            }
+            break;
+            case Command::kA: {
+                twist_.angular.z = 0;
+            }
+            break;
+            default:
+                ROS_ERROR("Unknown error");
+                break;
+        }
+        pub_.publish(twist_);
+    };
+
+};
+
+int main(int argc, char* argv[]){
+    ros::init(argc, argv, "velocity_controller");
+    ros::NodeHandle n;
+    ROS_INFO("Node %s starts", ros::this_node::getName().c_str());
+    Publisher pub = n.advertise<Twist>("/velocity_controller/cmd_vel", 4);
+    VelocityController vel_ctrl(0.0, 0.0, 1.0/16.0, 1.0/8.0, pub);
+    Subscriber sub = n.subscribe("/joystick_server/teleop", 4, &VelocityController::JoystickCallback, &vel_ctrl);
+    ros::spin();
+    return 0;
+}
