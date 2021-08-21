102eb4f662a7ac793b3e1948e327221f2b227fc8
iago <polarbolide@gmail.com>
2021-08-21 23:15:10 +0200

    feat: Reset mode 1 register

--- src/motion_driver/src/motion_driver_node.cpp
+++ src/motion_driver/src/motion_driver_node.cpp
@@ -33,7 +33,7 @@ private:
 
     void ResetRegisters(){
         // Reset MODE1 register
-        i2cWriteByteData(i3c_id_, 0, 0);
+        i2cWriteByteData(i2c_id_, 0, 0);
 
         // Reset control registers.
         for (int reg = 0x6; reg <= 0x45; reg++){
